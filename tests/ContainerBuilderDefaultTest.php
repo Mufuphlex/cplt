1008ddb7c5ec89fb876687232120e1a96daba69b
AlexMiroshnikov <AlexMiroshnikov>
2016-04-09 15:54:44 +0700

    Extend coverage for ContainerBuilder

--- tests/ContainerBuilderDefaultTest.php
+++ tests/ContainerBuilderDefaultTest.php
@@ -4,19 +4,41 @@ namespace Mufuphlex\Tests\Cplt;
 
 use Mufuphlex\Cplt\ContainerBuilderDefault;
 
-class ContainerBuilderDefaultTest extends \PHPUnit_Framework_TestCase
+class ContainerBuilderDefaultTest extends TestCase
 {
     public function testBuild()
+    {
+        $tokenizer = $this->makeTokenizer();
+        $sourceData = 'Some text is here';
+        $builder = new ContainerBuilderDefault($tokenizer);
+        $container = $builder->build($sourceData);
+        $this->assertInstanceOf('\Mufuphlex\Cplt\ContainerInterface', $container);
+    }
+
+    /**
+     * @dataProvider notStringDataProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testBuildFailsOnInvalidSourceData($sourceData)
+    {
+        $tokenizer = static::getMock('\Mufuphlex\Textonic\Tokenizer\TokenizerInterface');
+        $builder = new ContainerBuilderDefault($tokenizer);
+        $builder->build($sourceData);
+    }
+
+    private function makeTokenizer()
     {
         $tokenizer = static::getMock('\Mufuphlex\Textonic\Tokenizer\TokenizerInterface');
         $tokenizer
             ->expects(static::once())
             ->method('tokenize')
-            ->willReturn(array());
+            ->willReturn(array(
+                'Some',
+                'text',
+                'is',
+                'here',
+            ));
 
-        $sourceData = 'Some text is here';
-        $builder = new ContainerBuilderDefault($tokenizer);
-        $container = $builder->build($sourceData);
-        $this->assertInstanceOf('\Mufuphlex\Cplt\ContainerInterface', $container);
+        return $tokenizer;
     }
 }
