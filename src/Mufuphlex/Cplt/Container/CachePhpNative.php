70c7c0d2892ef021816ef6db7ec97bd7b2e3d2c5
AlexMiroshnikov <AlexMiroshnikov>
2016-04-09 22:53:52 +0700

    Add Cached Cplt Daemon

---
+++ src/Mufuphlex/Cplt/Container/CachePhpNative.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Mufuphlex\Cplt\Container;
+
+/**
+ * Class CachePhpNative
+ * @package Mufuphlex\Cplt\Container
+ */
+class CachePhpNative implements CacheInterface
+{
+    /** @var array */
+    private $storage = array();
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $expirationTime
+     * @return mixed
+     */
+    public function set($key, $value, $expirationTime = 0)
+    {
+        $this->storage[$key] = array(
+            'd' => $value,
+            'e' => ($expirationTime ? time() + $expirationTime : 0),
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (!isset($this->storage[$key])) {
+            return null;
+        }
+
+        if ($this->storage[$key]['e'] && (time() > $this->storage[$key]['e'])) {
+            return null;
+        }
+
+        return $this->storage[$key]['d'];
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function delete($key)
+    {
+        if (isset($this->storage[$key])) {
+            unset($this->storage[$key]);
+        }
+    }
+
+    /**
+     * @param  void
+     * @return $this
+     */
+    public function clear()
+    {
+        $this->storage = array();
+        return $this;
+    }
+}
