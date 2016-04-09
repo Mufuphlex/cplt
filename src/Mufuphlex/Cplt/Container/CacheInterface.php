a50da021c048327ea4d637f7149526eddfe567c6
AlexMiroshnikov <ilex@nigma.ru>
2016-04-09 23:29:36 +0700

    Feature/caching container (#4)
    * Add Cached Cplt Daemon
    
    * Updated factory test
    
    * Updated factory test
    
    * Test native cache
    
    * Finalize tests

---
+++ src/Mufuphlex/Cplt/Container/CacheInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mufuphlex\Cplt\Container;
+
+/**
+ * Interface CacheInterface
+ * @package Mufuphlex\Cplt\Container
+ */
+interface CacheInterface
+{
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $expirationTime
+     * @return mixed
+     */
+    public function set($key, $value, $expirationTime = 0);
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key);
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function delete($key);
+
+    /**
+     * @param  void
+     * @return mixed
+     */
+    public function clear();
+}
