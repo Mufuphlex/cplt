34b085b9a55be6ebf400837d3917dda4ace5fbca
AlexMiroshnikov <AlexMiroshnikov>
2016-04-09 15:41:01 +0700

    Implement ContainerBuilder

--- src/Mufuphlex/Cplt/ContainerBuilderDefault.php
+++ src/Mufuphlex/Cplt/ContainerBuilderDefault.php
@@ -9,5 +9,26 @@ namespace Mufuphlex\Cplt;
  */
 class ContainerBuilderDefault implements ContainerBuilderInterface
 {
+    /** @var \Mufuphlex\Textonic\Tokenizer\TokenizerInterface */
+    protected $tokenizer;
 
+    /**
+     * @param mixed $sourceData
+     * @return ContainerInterface
+     */
+    public function build($sourceData)
+    {
+        if (!is_string($sourceData)) {
+            throw new \InvalidArgumentException('$text must be a string');
+        }
+
+        $tokens = $this->tokenizer->tokenize($sourceData);
+        $container = new Container();
+
+        foreach ($tokens as $token) {
+            $container->addToken($token);
+        }
+
+        return $container;
+    }
 }
