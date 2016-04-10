befcb731b820f2b86cdecc0f716f3a6d94889933
AlexMiroshnikov <AlexMiroshnikov>
2016-04-10 11:21:35 +0700

    Smallize demo

--- demo/initiator.php
+++ demo/initiator.php
@@ -2,12 +2,11 @@
 require_once '../vendor/autoload.php';
 require_once 'demo.php';
 
-$port = getPort($argv);
 $factoryMethod = getFactoryMethod($argv);
 
 $text = file_get_contents('./data/where-love-is-there-god-is-also.txt');
 //$text = file_get_contents('./data/war-and-peace.txt');
 
-$cplt = \Mufuphlex\Cplt\Factory::$factoryMethod($text, $port);
+$cplt = \Mufuphlex\Cplt\Factory::$factoryMethod($text, getPort($argv));
 echo "\nMemory usage (current/peak), Kb: ".memory_get_usage(true).' / '.memory_get_peak_usage(true)."\n";
 $cplt->run();
